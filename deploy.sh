ab81a7f320784cb1146a0cbdaa2349347fbb8a81
xujianrong <xujianrong@leateckgroup.com>
2023-07-04 18:40:14 +0800

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+cd /home/ubuntu/app || return 1
+echo "进入目标目录，并构建构建镜像！"
+docker build -t snowflyaway/shadow:latest .
+
+cid=$(docker ps -a |grep "$IMAGE_NAME" | awk '{ print $1}')
+if [ "$cid" != "" ]; then
+  docker stop "$cid";
+  docker rm "$cid";
+fi
+echo "检查是否有运行的容器！"
+
+IMAGE_NAME=shadow
+docker run --name "$IMAGE_NAME" -p 8280:8080 -d --link my_mysql:mysql -e JAVA_OPTS="$SHADOW_OPTS" -e DB_USERNAME="$DB_USERNAME" -e DB_PASSWORD="$DB_PASSWORD" snowflyaway/shadow
+sleep 5s;
+
+echo "$IMAGE_NAME 部署结束！"
+exit 0
