206dc3c980922f67548d3ebfa410c37ed2522595
jianrong <137472591@qq.com>
2023-09-05 23:19:31 +0800

    修改数据表初始化语句

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -19,21 +19,3 @@ CREATE TABLE IF NOT EXISTS `sys_user`
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4 COMMENT ='用户表';
 
-CREATE TABLE IF NOT EXISTS `user_data`
-(
-    `id`          int NOT NULL AUTO_INCREMENT COMMENT '主键',
-    `user_id`     varchar(64)  DEFAULT NULL COMMENT '用户ID',
-    `user_name`   varchar(64)  DEFAULT NULL COMMENT '名称',
-    `sex_type`    varchar(8)   DEFAULT NULL COMMENT '性别',
-    `birth_day`   date         DEFAULT NULL COMMENT '生日',
-    `is_freeze`   int          DEFAULT NULL COMMENT '是否冻结',
-    `user_status` int          DEFAULT NULL COMMENT '用户状态',
-    `email`       varchar(255) DEFAULT NULL COMMENT '邮箱',
-    `phone_no`    varchar(255) DEFAULT NULL COMMENT '手机',
-    `create_time` datetime     DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `update_time` datetime     DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间',
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `user_name_UK` (`user_name`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4 COMMENT ='用户信息表';
-
