ac806f75b48fe14ddf6a5aadd34a41975ada012b
xujianrong <xujianrong@leateckgroup.com>
2023-09-21 16:27:35 +0800

    add service

---
+++ src/main/java/com/github/shadow/service/IZhihuContentService.java
@@ -0,0 +1,16 @@
+package com.github.shadow.service;
+
+import com.github.shadow.entity.ZhihuContent;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 内容 服务类
+ * </p>
+ *
+ * @author xuwen
+ * @since 2023-09-20
+ */
+public interface IZhihuContentService extends IService<ZhihuContent> {
+
+}
---
+++ src/main/java/com/github/shadow/service/impl/ZhihuContentServiceImpl.java
@@ -0,0 +1,20 @@
+package com.github.shadow.service.impl;
+
+import com.github.shadow.entity.ZhihuContent;
+import com.github.shadow.mapper.ZhihuContentMapper;
+import com.github.shadow.service.IZhihuContentService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 内容 服务实现类
+ * </p>
+ *
+ * @author xuwen
+ * @since 2023-09-20
+ */
+@Service
+public class ZhihuContentServiceImpl extends ServiceImpl<ZhihuContentMapper, ZhihuContent> implements IZhihuContentService {
+
+}
